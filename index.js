3ad590857e0bc4a7b8f1d3bb1c03f3e44f685672
Benjamin <ben.chesser19@gmail.com>
2023-03-02 16:34:29 -0800

    finished part 1 of project

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imgSrc, left, bottom) {
+    let newElement = document.createElement('img')
+newElement.src = imgSrc
+newElement.style.position = 'fixed'
+newElement.style.left = left 
+newElement.style.bottom = bottom
+document.body.append(newElement)
+return newElement
+}
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+// let sword = document.createElement('img')
+// sword.src = 'assets/sword.png'
+// sword.style.position = 'fixed'
+// sword.style.left = '500px'
+// sword.style.bottom = '405px'
+// document.body.append(sword)
+
+// sword.addEventListener('click', function(){
+//     sword.remove()
+// })
+
+let sword = newImage('assets/sword.png', '500px', '405px')
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
